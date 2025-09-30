d7ee10795da71c0384e7da811bced3ef2a5c2e52
tafita98 <tafitaprospere@gmail.com>
2025-09-30 14:09:45 +0300

    route sur tresor

---
+++ src/pages/clients/tresorie/BudgetTracker.jsx
@@ -0,0 +1,10 @@
+const BudgetTracker = () => {  return (
+        <div className="text-gray-600"
+          
+        >
+          trezo
+        </div>
+    );
+  };
+
+  export default BudgetTracker
--- src/routes/ClientRoute.jsx
+++ src/routes/ClientRoute.jsx
@@ -1,6 +1,7 @@
 import ProjetLayoutClient from "../layouts/clients/projet/ProjetLayoutAdmin";
 import DashboardView from "../pages/clients/dashboard/DashboardView";
 import ProjetClient from "../pages/clients/projets/ProjetClient";
+import BudgetTracker from "../pages/clients/tresorie/BudgetTracker";
 
 const ClientRoute = {
   path: "/client",
@@ -17,6 +18,10 @@ const ClientRoute = {
     {
       path: "dashboard",
       element: <DashboardView />,
+    },
+    {
+      path: "trezo",
+      element: <BudgetTracker />,
     },
   ],
 };
